b4170aa5e5da03023c29d42da083368fb72361ab
anti1hero1 <nikitadanikita@gmail.com>
2023-03-08 20:16:03 +0800

    f

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
-console.log('Mesto JS :)')
+console.log('Место JS :)')
 
 const popupWindow = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close');
 const saveButton = document.querySelector('.popup__button-save');
+const cardLike = document.querySelector('.element__like');
 
 let formElement = document.querySelector('.popup__content');
 let nameAuthor = document.querySelector('.profile__info-title');
@@ -11,13 +12,18 @@ let jobAuthor = document.querySelector('.profile__info-subtitle');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_job');
 
-function openPopup () {
+function openPopup() {
     popupWindow.classList.add('popup_opened');
     nameInput.value = nameAuthor.innerHTML;
     jobInput.value = jobAuthor.innerHTML;
 };
 
-function handleFormSubmit (evt) {
+function toggleButton() {
+    cardLike.classList.toggle('element__like_active');
+};
+
+
+function handleFormSubmit(evt) {
     evt.preventDefault();
     nameAuthor.textContent = nameInput.value;
     jobAuthor.textContent = jobInput.value;
@@ -25,6 +31,6 @@ function handleFormSubmit (evt) {
 };
 
 editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', function () {popupWindow.classList.remove('popup_opened')});
-formElement.addEventListener('submit', handleFormSubmit); 
-
+closeButton.addEventListener('click', function () { popupWindow.classList.remove('popup_opened') });
+formElement.addEventListener('submit', handleFormSubmit);
+cardLike.addEventListener('click', toggleButton);
