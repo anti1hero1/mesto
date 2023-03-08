fbd721b3ac3c6e08e28fb0e1d22e38bfdb22ce6a
anti1hero1 <nikitadanikita@gmail.com>
2023-03-08 19:00:58 +0800

    popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,30 @@
-console.log('Mесто JS :)')
+console.log('Mesto JS :)')
+
+const popupWindow = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close');
+const saveButton = document.querySelector('.popup__button-save');
+
+let formElement = document.querySelector('.popup__content');
+let nameAuthor = document.querySelector('.profile__info-title');
+let jobAuthor = document.querySelector('.profile__info-subtitle');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
+
+function openPopup () {
+    popupWindow.classList.add('popup_opened');
+    nameInput.value = nameAuthor.innerHTML;
+    jobInput.value = jobAuthor.innerHTML;
+};
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    nameAuthor.textContent = nameInput.value;
+    jobAuthor.textContent = jobInput.value;
+    popupWindow.classList.remove('popup_opened');
+};
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', function () {popupWindow.classList.remove('popup_opened')});
+formElement.addEventListener('submit', handleFormSubmit); 
 
